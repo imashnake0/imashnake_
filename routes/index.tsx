b77ef877e22244cfaff2388e7ed973b810815388
imashnake0 <kamaleshpaluru07@gmail.com>
2023-03-13 23:21:25 -0400

    Cleanup

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,23 +1,15 @@
-import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>imashnake_</title>
-      </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/shnake_.svg"
-          class="w-32 h-32"
-          alt="imashnake_"
-        />
-        <p class="my-6">
-          hi guy.
-        </p>
-        <Counter start={3} />
-      </div>
+      <head>
+        <title>
+          imashnake_
+        </title>
+      </head>
+
+      <body>
+        Hello
+      </body>
     </>
   );
 }

5c807df2d70765de207b859392545d8043d92cef
imashnake0 <krpaluru@uwaterloo.ca>
2025-09-13 04:31:08 -0400

    re: fix config

--- _config.ts
+++ _config.ts
@@ -6,9 +6,9 @@ const site = lume({
 
 site.copy("/index.html")
 site.copy("/imashnake.css")
-site.copy("/static/favicon.ico")
+site.copy("/favicon.ico")
 site.copy("/static/harbor_front.jpg")
-site.copy("/static/love_rock.jpg")
+site.copy("/static/love_lock.jpg")
 site.copy("/static/ocean_morning.jpg")
 site.copy("/static/ocean.jpg")
 site.copy("/static/rock_garden.jpg")

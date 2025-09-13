cf04c213a8b3cd68e72bbaa77986d698933273f2
imashnake0 <krpaluru@uwaterloo.ca>
2025-09-13 04:29:31 -0400

    add icon back

--- _config.ts
+++ _config.ts
@@ -6,6 +6,7 @@ const site = lume({
 
 site.copy("/index.html")
 site.copy("/imashnake.css")
+site.copy("/static/favicon.ico")
 site.copy("/static/harbor_front.jpg")
 site.copy("/static/love_rock.jpg")
 site.copy("/static/ocean_morning.jpg")

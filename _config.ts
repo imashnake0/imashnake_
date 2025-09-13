120017f88e7c31cae20d060b533362b736499a19
imashnake0 <krpaluru@uwaterloo.ca>
2025-09-13 04:27:05 -0400

    fix config
    https://lume.land/docs/getting-started/working-with-assets/

--- _config.ts
+++ _config.ts
@@ -4,7 +4,13 @@ const site = lume({
     src: "./src"
 });
 
-site.copy("index.html")
-site.copy("imashnake.css")
+site.copy("/index.html")
+site.copy("/imashnake.css")
+site.copy("/static/harbor_front.jpg")
+site.copy("/static/love_rock.jpg")
+site.copy("/static/ocean_morning.jpg")
+site.copy("/static/ocean.jpg")
+site.copy("/static/rock_garden.jpg")
+site.copy("/static/speed.jpg")
 
 export default site;

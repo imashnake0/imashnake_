4e0922118329c3453c359160ff7c5c9cad402ef1
imashnake0 <krpaluru@uwaterloo.ca>
2026-01-09 13:22:07 -0500

    fix

--- _config.ts
+++ _config.ts
@@ -1,4 +1,5 @@
 import lume from "lume/mod.ts";
+import blog from "https://deno.land/x/lume_theme_simple_blog@v0.15.6/mod.ts";
 
 const site = lume({
     src: "./src"
@@ -6,6 +7,7 @@ const site = lume({
 
 site.copy("/index.html")
 site.copy("/imashnake.css")
+site.copy("/_data.yml")
 site.copy("/favicon.ico")
 site.copy("/static/harbor_front.jpg")
 site.copy("/static/love_lock.jpg")
@@ -13,5 +15,7 @@ site.copy("/static/ocean_morning.jpg")
 site.copy("/static/ocean.jpg")
 site.copy("/static/rock_garden.jpg")
 site.copy("/static/speed.jpg")
+
+site.use(blog());
 
 export default site;
